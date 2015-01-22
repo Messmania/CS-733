0d5c6d5eb106dc43377b7a07fbf5afc0738d3678
Monika Sahai <moni.coolx@gmail.com>
2015-01-23 00:40:16 +0530

    Modified test file to include few more cases

--- assignment1/ServerMemCache_test.go
+++ assignment1/ServerMemCache_test.go
@@ -6,61 +6,56 @@ import (
 )
 
 func TestClient(t *testing.T) {
-	//t.Parallel()
 	go Server()
 	status := make(chan string)
-	//expected output
-
 	set1 := "set abc 20 10\r\nabcdefjg\r\n"
-	/*
 	set2 := "set bcd 30 5\r\nefghi\r\n"
-	getm1 := "getm bcd\r\n"
-	cas1 := "cas bcd 30 7881 20\r\nnewValue\r\n"
-	del1 := "delete abc\r\n"
-	getm2 := "getm abc\r\n"
-	*/
-
+	getm1 := "getm abc\r\n"
+	cas1 := "cas bcd 30 7887 20\r\nnewValue\r\n"
+	del1 := "delete bcd\r\n"
+	getm2 := "getm bcd\r\n"
+	
 	Eset1 := "OK 8081\r\n"
-/*	
 	Eset2 := "OK 7887\r\n"
-	Egetm1 := "VALUE 7887 30 5\r\nefghi\r\n"
-	Ecas1 := "ERRNOTFOUND"
-	Edel1 := "DELETED"
-	Egetm2 := "ERRNOTFOUND"
-*/
-	go Client(status, set1, "set1")
-/*
-	go Client(status, set2, "set2")
-	go Client(status, getm1, "getm1")
-	go Client(status, cas1, "cas1")
-	go Client(status, del1, "del1")
-	go Client(status, getm2, "getm2")
-*/
-	Rset1 := <-status
+	Egetm1 := "VALUE 8081 20 10\r\nabcdefjg\r\n"
+	Ecas1 := "OK 7887\r\n"
+	Edel1 := "DELETED\r\n"
+	Egetm2 := "ERRNOTFOUND\r\n"
 
+	
+	go Client(status, set1, "set1")
+	Rset1 := <-status
 	if strings.EqualFold(Eset1, Rset1) != true {
-		t.Errorf("Received and expected values are:\r\n%v%v", Rset1, Eset1)
+		t.Error("Expected and Received values are:\r\n",Eset1,Rset1)
 	}
-/*
+
+	go Client(status, set2, "set2") 
 	Rset2 := <-status
 	if strings.EqualFold(Eset2, Rset2) != true {
-		t.Errorf("Received and expected values are:\r\n%v%v", Rset2, Eset2)
+		t.Error("Expected and Received values are:\r\n",Eset2, Rset2)
 	}
+
+	go Client(status, getm1, "getm1")
 	Rgetm1 := <-status
 	if strings.EqualFold(Egetm1, Rgetm1) != true {
-		t.Errorf("Received and expected values are:\r\n%v%v", Rgetm1, Egetm1)
+		t.Error("Expected and Received values are:\r\n", Egetm1, Rgetm1)
 	}
+	
+	go Client(status, cas1, "cas1")
 	Rcas1 := <-status
 	if strings.EqualFold(Ecas1, Rcas1) != true {
-		t.Errorf("Received and expected values are:\r\n%v%v", Rcas1, Ecas1)
+		t.Error("Expected and Received values are:\r\n", Ecas1, Rcas1)
 	}
+	
+	go Client(status, del1, "del1")
 	Rdel1 := <-status
 	if strings.EqualFold(Edel1, Rdel1) != true {
-		t.Errorf("Received and expected values are:\r\n%v%v", Rdel1, Edel1)
+		t.Error("Expected and Received values are:\r\n", Edel1,Rdel1)
 	}
+	
+	go Client(status, getm2, "getm2")
 	Rgetm2 := <-status
 	if strings.EqualFold(Egetm2, Rgetm2) != true {
-		t.Errorf("Received and expected values are:\r\n%v%v", Rgetm2, Egetm2)
+		t.Error("Expected and Received values are:\r\n", Egetm2, Rgetm2)
 	}
-*/
 }
