ffa1282ac6273ba59b2a14c04202f37603048bb2
Monika Sahai <moni.coolx@gmail.com>
2015-01-25 08:22:50 +0530

    Added test cases

--- assignment1/ServerMemCache.go
+++ assignment1/ServerMemCache.go
@@ -1,232 +1,259 @@
 /* Trying to make an echo server
- */
+*/
 
 package Server
-
-//package main
 import (
 	"fmt"
-	"math"
-	"math/rand"
-	"net"
 	"os"
-	"strconv"
+	"net"
 	"strings"
-	"sync"
 	"time"
-)
+	"strconv"
+	"math/rand"
+	"math"
+	"sync"
+	)
 
-var secs time.Duration = time.Duration(math.Pow10(9))
-var m = make(map[string]Data)
-var globMutex = &sync.Mutex{}
+var secs time.Duration=time.Duration(math.Pow10(9))
+var m=make(map[string]Data)
+var globMutex=&sync.Mutex{}
 
-type Data struct {
-	value    string
-	version  int64
+type Data struct  {
+	value string
+	version int64 		
 	numbytes int64
-	setTime  int64
-	expiry   int64
-	dbMutex  *sync.Mutex
+	setTime int64
+	expiry int64		
+	dbMutex *sync.Mutex
+	
 }
 
-func Server() {
-	//fmt.Println("Entering server\n")
-	service := ":9000"
-	tcpaddr, err := net.ResolveTCPAddr("tcp", service) // Try eliminating this later, it is not needed
+
+func Server(){
+	//fmt.Println("Entering server\n")	
+	service:=":9000"
+	tcpaddr,err := net.ResolveTCPAddr("tcp",service) // Try eliminating this later, it is not needed
 	checkErr(err)
 	//fmt.Println("Resolve done\n")
-	listener, err := net.ListenTCP("tcp", tcpaddr)
-	checkErr(err)
+	listener,err := net.ListenTCP("tcp",tcpaddr)
+	checkErr(err)	
 	//fmt.Println("Listen done\n")
 	for {
-		//fmt.Println("Entering for\n")
-		conn, err := listener.Accept()
-		if err != nil {
+			//fmt.Println("Entering for\n")
+			conn,err := listener.Accept()
+			if err!=nil {
 			//fmt.Println("Accept failed\n")
 			continue
-		}
-		//fmt.Println("Calling handleClient\n")
-		go handleClient(conn, m)
-		//conn.Close()
+			}
+			go handleClient(conn,m)
 	}
 }
 
-func handleClient(conn net.Conn, m map[string]Data) {
-	//fmt.Println("In handleClient\n")
-	var buf [512]byte
-	n, err := conn.Read(buf[0:])
-	if err != nil {
-		fmt.Println("In read error\n")
-		return
-	}
-	sr := ""
-	/*Convert command read from conn to array of strings then read them separately	*/
-	str := string(buf[0:n])
-	line := strings.Split(str, "\r\n")
-	cmd := strings.Fields(line[0])
-	value := line[1] //TO DO--Check if 2nd line has wrong no.of args or ignore
-	l := len(cmd)
-	key := cmd[1]
-	op := strings.ToLower(cmd[0])
-	switch op {
-	case "set":
-		// do argument count check
-		if l == 4 {
-			exp, _ := strconv.ParseInt(cmd[2], 0, 64)
-			numb, _ := strconv.ParseInt(cmd[3], 0, 64)
-			ver := int64(rand.Intn(10000))
-			//fmt.Println("ver and randno. is", ver,rand.Intn(10000))
-			//d,exist :=m[key]
-			//if exist==false {
-			globMutex.Lock()
-			m[key] = Data{value, ver, numb, time.Now().Unix(), exp, &sync.Mutex{}}
-			d := m[key]
-			if exp > 0 {
-				expInSec := secs * time.Duration(exp)
-				time.AfterFunc(expInSec, func() {
-					checkAndExpire(m, key, exp, d.setTime)
-				})
-				//fmt.Printf("d.version type and value: %T %T %v\n", ver,d.version,d.version)
-				globMutex.Unlock()
-				//}
-			} /*else {																		//update the values: NO NEED as set will overwrite
-			//Use dbChannel for this key
-				d.dbMutex.Lock()
-				d.value = value
-				d.expiry = exp
-				d.numbytes = numb
-				if exp>0 {
-					expInSec :=secs*time.Duration(exp)
-					time.AfterFunc(expInSec,func(){
-						checkAndExpire(m,key,exp,d.setTime)
-					})
-				}
-			d.dbMutex.Unlock()
-			}	*/
-			//fmt.Println("before sr, d.version is", d.version)
-			sr = "OK " + strconv.FormatInt(d.version, 10) + "\r\n"
-		} else if l == 5 { //noreply
-			sr = ""
-		} else {
-			sr = "ERR_CMD_ERR \r\n" //wrong command line format
-		}
-	case "get":
-		if l == 2 {
-			//do get processing
-			d, exist := m[key]
-			if exist != false {
-				d.dbMutex.Lock()
-				numStr := strconv.FormatInt(d.numbytes, 10)
-				sr = "VALUE " + numStr + "\r\n" + d.value + "\r\n"
-				d.dbMutex.Unlock()
-			} else {
-				sr = "ERRNOTFOUND\r\n"
+
+func handleClient(conn net.Conn,m map[string]Data) {
+		for true{
+			var buf [512]byte
+			n, err := conn.Read(buf[0:])
+			if err != nil {
+				return
 			}
-		} else {
-			sr = "ERR_CMD_ERR \r\n"
-		}
-	case "getm":
-		if l == 2 {
-			//do getm processing
-			d, exist := m[key]
-			if exist != false {
-				d.dbMutex.Lock()
-				remExp := d.expiry - (time.Now().Unix() - d.setTime)
-				verStr := strconv.FormatInt(d.version, 10)
-				sr = "VALUE " + verStr + " " + strconv.FormatInt(remExp, 10) + " " + strconv.FormatInt(d.numbytes, 10) + "\r\n" + d.value + "\r\n"
-				d.dbMutex.Unlock()
-			} else {
-				sr = "ERRNOTFOUND\r\n"
-			}
-		} else {
-			sr = "ERR_CMD_ERR \r\n"
-		}
-	case "cas":
-		if l == 5 {
-			d, exist := m[key]
-			if exist != false {
-				//d.dbChan<-"start"
-				d.dbMutex.Lock()
-				oldVersion := strconv.FormatInt(d.version, 10)
-				newVersion := cmd[3]
-				numbytes := cmd[4]
-				if newVersion == oldVersion {
-					//Replace the value as old and new version are same
-					newVFloat, _ := strconv.ParseInt(newVersion, 10, 64) //check their errors as well!!
-					numBInt, _ := strconv.ParseInt(numbytes, 10, 64)
-					exp, _ := strconv.ParseInt(cmd[2], 0, 64)
-					m[key] = Data{value, newVFloat, numBInt, time.Now().Unix(), exp, &sync.Mutex{}}
-					d.dbMutex.Unlock()
-					sr = "OK " + newVersion + "\r\n"
+			sr :=""
+			/*Convert command read from conn to array of strings then read them separately	*/
+			str := string(buf[0:n])
+			line := strings.Split(str, "\r\n")
+			cmd := strings.Fields(line[0])
+			op:= strings.ToLower(cmd[0])
+			value:= line[1]			//TO DO--Check if 2nd line has wrong no.of args or ignore
+			l:= len(cmd)
+			key := cmd[1]
+		
+			switch op {
+			case "set":
+			if l == 4 {
+					exp,_ :=strconv.ParseInt(cmd[2],0,64)
+					numb,_:=strconv.ParseInt(cmd[3],0,64)
+					ver:= int64(rand.Intn(10000))
+					globMutex.Lock()
+					m[key] = Data{value,ver,numb,time.Now().Unix(),exp,&sync.Mutex{}} 
+					d:=m[key]
+					if exp>0 {
+						expInSec :=secs*time.Duration(exp)
+						time.AfterFunc(expInSec,func(){
+							checkAndExpire(m,key,exp,d.setTime)
+						})
+					}	
+					globMutex.Unlock() 			
+					sr ="OK "+strconv.FormatInt(d.version,10)+"\r\n"
+				} else if l == 5 && cmd[4]=="noreply" { 
+					exp,_ :=strconv.ParseInt(cmd[2],0,64)
+					numb,_:=strconv.ParseInt(cmd[3],0,64)
+					ver:= int64(rand.Intn(10000))
+					globMutex.Lock()
+					m[key] = Data{value,ver,numb,time.Now().Unix(),exp,&sync.Mutex{}} 
+					d:=m[key]
+					if exp>0 {
+						expInSec :=secs*time.Duration(exp)
+						time.AfterFunc(expInSec,func(){
+							checkAndExpire(m,key,exp,d.setTime)
+						})
+					}	
+					globMutex.Unlock() 			
+					sr =""
+				}else {
+					sr = "ERR_CMD_ERR\r\n" 										//wrong command line format
+				}	
+			case "get":
+				if l==2 {
+				//do get processing
+					d,exist := m[key]
+					if exist!=false {
+						d.dbMutex.Lock()
+						numStr := strconv.FormatInt(d.numbytes,10)
+						sr="VALUE "+numStr+"\r\n"+d.value+"\r\n" 		
+						d.dbMutex.Unlock()
+					}else{
+						sr= "ERRNOTFOUND\r\n"
+					}
 				} else {
-					sr = "ERR_VERSION\r\n"
+				sr = "ERR_CMD_ERR\r\n"
 				}
-			} else {
-				sr = "ERRNOTFOUND\r\n"
+			case "getm":
+				if(l==2){
+				//do getm processing
+					d,exist := m[key]
+					if exist!=false {
+						d.dbMutex.Lock()
+							remExp := d.expiry -( time.Now().Unix() - d.setTime)
+							verStr :=strconv.FormatInt(d.version,10)
+						sr = "VALUE "+verStr+" "+strconv.FormatInt(remExp,10)+" "+strconv.FormatInt(d.numbytes,10)+"\r\n"+d.value+"\r\n"
+						d.dbMutex.Unlock()		
+					}else{
+						sr= "ERRNOTFOUND\r\n"
+					}
+				} else{
+				sr = "ERR_CMD_ERR\r\n"
+				}
+			case "cas":
+				if l==5 {
+					d,exist := m[key]
+					if exist!=false {
+						d.dbMutex.Lock()
+						oldVersion:=strconv.FormatInt(d.version,10)
+						newVersion:=cmd[3]
+						numbytes:=cmd[4]
+						if newVersion == oldVersion {
+						//Replace the value as old and new version are same
+						newVFloat,_:=strconv.ParseInt(newVersion,10,64) 		//check their errors as well!!
+						numBInt,_:=strconv.ParseInt(numbytes,10,64)
+						exp,_ :=strconv.ParseInt(cmd[2],0,64)
+						m[key]=Data{value,newVFloat,numBInt,time.Now().Unix(),exp,&sync.Mutex{}}
+						d.dbMutex.Unlock()
+						sr="OK "+newVersion+"\r\n"
+						}else {
+						sr= "ERR_VERSION\r\n"
+						}
+					}else{
+						sr= "ERRNOTFOUND\r\n"
+					}
+				}else if l==6 && cmd[5]=="noreply" {
+					sr = ""
+				}else{
+					sr = "ERR_CMD_ERR\r\n"
+					}		
+			case "delete":
+				d,exist := m[key]
+				if exist!=false {
+					d.dbMutex.Lock()
+					delete(m,key)
+					d.dbMutex.Unlock()
+					sr = "DELETED\r\n"
+				}else{
+					sr= "ERRNOTFOUND\r\n"		
+					}
+			default:
+				sr = "ERRINTERNAL\r\n"
+				}
+			_, err2 := conn.Write([]byte(sr))
+			if err2 != nil {
+				return
 			}
-		} else if l == 6 && cmd[5] == "noreply" {
-			sr = ""
-		} else {
-			sr = "ERR_CMD_ERR \r\n"
-		}
-	case "delete":
-		//fmt.Println("Inside delete")
-		d, exist := m[key]
-		if exist != false {
-			d.dbMutex.Lock()
-			delete(m, key)
-			d.dbMutex.Unlock()
-			sr = "DELETED\r\n"
-		} else {
-			sr = "ERRNOTFOUND\r\n"
-		}
-	default:
-		sr = "ERRINTERNAL\r\n"
-	}
-	//fmt.Println("at the end of handleClient\n")
-
-	_, err2 := conn.Write([]byte(sr))
-	if err2 != nil {
-		return
 	}
 }
 
-func checkErr(err error) {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-		os.Exit(1)
+
+
+func checkErr(err error){
+	if err!=nil {
+	fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+	os.Exit(1)
 	}
 }
 
-func checkAndExpire(m map[string]Data, key string, oldExp int64, setTime int64) {
-	d, err := m[key]
+func checkAndExpire(m map[string]Data,key string,oldExp int64,setTime int64) {
+d,exist := m[key]
 	absOldExp := setTime + oldExp
-	absNewExp := setTime + d.expiry
-	if err != false {
-		return
+	absNewExp := setTime + d.expiry	
+	if exist==false{
+	return	
 	}
-	if absOldExp != absNewExp {
-		return
-	} else {
-		delete(m, key)
-		return
+	if absOldExp!=absNewExp {	
+	return 
+	}else{	
+	delete(m,key)
+	return
 	}
 
 }
 
-func Client(ch chan string, strEcho string, c string) (reply string) {
+
+func Client(ch chan string,strEcho string, c string) {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", ":9000")
 	checkErr(err)
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	checkErr(err)
-
-	conn.Write([]byte(strEcho)) //Writing the message to connection stream which server can read
-	var rep [512]byte
-	n, err1 := conn.Read(rep[0:])
-	checkErr(err1)
-	reply = string(rep[0:n])
-	//fmt.Printf("Reply of %v is: %v \n",c,reply[0:n])
+	cmd:=SeparateCmds(strEcho)
+	for i:=0;i<len(cmd);i++ {
+		conn.Write([]byte(cmd[i]))		//Writing the message to connection stream which server can read
+		var rep [512]byte
+		n,err1 := conn.Read(rep[0:])
+		checkErr(err1)	
+		reply:= string(rep[0:n])
+		//fmt.Printf("Reply of %v is: %v \n",c,reply[0:n])		//For testing manually
+		ch<-reply
+	}
 	conn.Close()
-	ch <- reply
+}
+
+
+func SeparateCmds(str string)(cmd []string) {
+	line:=strings.Split(str,"\r\n")
+	count:=len(line)-1
+	args:=make([][]string,count)
+	for i:=0;i<count;i++ {
+		args[i]=strings.Fields(line[i])
+	}
+	ptr:=0
+	cmd=make([]string,0)
+	var newC string
+	for j:=0;j<count;j++ {
+		op:= strings.ToLower(args[ptr][0])
+		if (op=="set") || (op=="cas") {
+			newC:=line[ptr]+"\r\n"+line[ptr+1]+"\r\n"
+			cmd=append(cmd,newC)
+			ptr=ptr+2
+			j++
+		}else if (op=="get")||(op=="getm")||(op=="delete"){
+			newC=line[ptr]+"\r\n"
+			cmd=append(cmd,newC)
+			ptr++		
+		}else if (op=="end") {
+			cmd=append(cmd,op)
+			break
+		}else {
+			newC=line[j]
+			cmd=append(cmd,newC)
+			ptr++
+		}
+	}
 	return
 }
--- assignment1/ServerMemCache_test.go
+++ assignment1/ServerMemCache_test.go
@@ -3,59 +3,216 @@ package Server
 import (
 	"strings"
 	"testing"
+	//"fmt"
+	"time"
 )
 
-func TestClient(t *testing.T) {
+func TestServer(t *testing.T){
 	go Server()
-	status := make(chan string)
+}
+
+
+//PASSED
+//Checking server's affirmative responses
+func TestResponse(t *testing.T){
+	t.Parallel()
 	set1 := "set abc 20 10\r\nabcdefjg\r\n"
 	set2 := "set bcd 30 5\r\nefghi\r\n"
 	getm1 := "getm abc\r\n"
 	cas1 := "cas bcd 30 7887 20\r\nnewValue\r\n"
+	get2 := "get bcd\r\n"
 	del1 := "delete bcd\r\n"
-	getm2 := "getm bcd\r\n"
+	
 	
 	Eset1 := "OK 8081\r\n"
 	Eset2 := "OK 7887\r\n"
 	Egetm1 := "VALUE 8081 20 10\r\nabcdefjg\r\n"
-	Ecas1 := "OK 7887\r\n"
+	Ecas1 := "OK 7887\r\n"	
+	Eget2 := "VALUE 20\r\nnewValue\r\n"
 	Edel1 := "DELETED\r\n"
-	Egetm2 := "ERRNOTFOUND\r\n"
+	E:=[]string{Eset1,Eset2,Egetm1,Ecas1,Eget2,Edel1}
 
+	cmd:=[]string{set1,set2,getm1,cas1,get2,del1}
+	cd:=[]string{"set1","set2","getm1","cas1","get2","del1"}
+	chann:=make([]chan string,5)
 	
-	go Client(status, set1, "set1")
-	Rset1 := <-status
-	if strings.EqualFold(Eset1, Rset1) != true {
-		t.Error("Expected and Received values are:\r\n",Eset1,Rset1)
+	//for initialing the array otherwise it is becoming nil
+	for k:=0;k<5;k++ {
+		chann[k]=make(chan string)
+	}
+	for i:=0;i<5;i++ {
+		go Client(chann[i],cmd[i],cd[i])
+	}
+	
+	var R [5]string
+	for j:=0;j<5;j++ {
+		R[j]=<-chann[j]
+		if R[j]!=E[j] {			//strings.EqualFold is failing for same strings..--check later about utf-8 coding
+			t.Error("Expected and Received values are:\r\n",E[j],R[j])
+		}
 	}
 
-	go Client(status, set2, "set2") 
-	Rset2 := <-status
-	if strings.EqualFold(Eset2, Rset2) != true {
-		t.Error("Expected and Received values are:\r\n",Eset2, Rset2)
-	}
-
-	go Client(status, getm1, "getm1")
-	Rgetm1 := <-status
-	if strings.EqualFold(Egetm1, Rgetm1) != true {
-		t.Error("Expected and Received values are:\r\n", Egetm1, Rgetm1)
-	}
-	
-	go Client(status, cas1, "cas1")
-	Rcas1 := <-status
-	if strings.EqualFold(Ecas1, Rcas1) != true {
-		t.Error("Expected and Received values are:\r\n", Ecas1, Rcas1)
-	}
-	
-	go Client(status, del1, "del1")
-	Rdel1 := <-status
-	if strings.EqualFold(Edel1, Rdel1) != true {
-		t.Error("Expected and Received values are:\r\n", Edel1,Rdel1)
-	}
-	
-	go Client(status, getm2, "getm2")
-	Rgetm2 := <-status
-	if strings.EqualFold(Egetm2, Rgetm2) != true {
-		t.Error("Expected and Received values are:\r\n", Egetm2, Rgetm2)
-	}
 }
+
+//PASSED =======but race conditions coming
+//To check expiration and remaining exp time in getm
+func TestExpiry(t *testing.T){
+	t.Parallel()
+	chann:=make([]chan string,3)
+	
+	//for initialing the array otherwise it is becoming nil
+	for k:=0;k<3;k++ {
+		chann[k]=make(chan string)
+	}
+	
+	//Commands
+	set1 := "set abc 2 10\r\nabcdefjg\r\n"
+	getm1 := "getm abc\r\n"
+
+	//Expected values
+	Eset1 := "OK"
+	Egetm1 := "VALUE 1 10\r\nabcdefjg\r\n"
+	Egetm2 := "ERRNOTFOUND\r\n"
+	E:=[]string{Eset1,Egetm1,Egetm2}
+
+	go Client(chann[0],set1,"set1")
+	time.Sleep(time.Second*1)
+	go Client(chann[1],getm1,"getm1")
+	time.Sleep(time.Second*1)
+	go Client(chann[2],getm1,"getm2")
+
+	var R [3]string
+
+	//Excluding hard coded version number	
+	Rset1 :=strings.Fields(<-chann[0])
+	R[0]= Rset1[0]
+	
+	RLine:= strings.Split(<-chann[1],"\r\n")
+	Rgetm1:=strings.Fields(RLine[0])
+	R[1]=Rgetm1[0]+" "+Rgetm1[2]+" "+Rgetm1[3]+"\r\n"+RLine[1]+"\r\n"
+	
+	R[2]=<-chann[2]
+	
+	
+	for j:=0;j<3;j++ {
+		if R[j]!=E[j] {			
+			t.Error("Expected and Received values are:\r\n",E[j],R[j])
+		}
+	}
+	
+}
+
+
+//PASSED
+func TestErrors(t *testing.T){
+	t.Parallel()
+	const n int=10
+	//=============For ERRNOTFOUND==========
+	get1:="get ms\r\n"
+	getm1:="getm ms\r\n"
+	cas1:="cas ms 2 2204 10\r\nmonikasaha\r\n"
+	del1:="delete ms\r\n"
+	
+
+	//============For ERR_CMD_ERR===========
+	set1:="SET ms 5 11 extra\r\nmonikasahai\r\n"
+	cas2:="cas a\r\n"
+	get2:="get ms ok\r\n"
+	
+	//===========For ERR_VERSION============
+	set2:="set ms 2 11\r\nmonikasahai\r\n"
+	cas3:="cas ms 2 1092 10\r\nnewMonika\r\n"
+	
+	//==========For ERR_INTERNAL============ Not correct logically
+	msg1:="del ms"
+
+
+	cmd:=[]string{get1,getm1,cas1,del1,get1,set1,cas2,get2,set2,cas3,msg1}
+	cd:=[]string{"get1","getm1","cas1","del1","get1","set1","cas2","get2","set2","cas3","msg1"}
+	err0:="ERRNOTFOUND\r\n"
+	err1:="ERR_CMD_ERR\r\n"
+	err2:="ERR_VERSION\r\n"
+	Rset1:="OK"	
+	err3:="ERR_INTERNAL\r\n"
+	E:=[]string{err0,err0,err0,err0,err0,err1,err1,err1,Rset1,err2,err3}
+
+	//Declare and initialize channels
+	chann:=make([]chan string,n)
+	for k:=0;k<n;k++ {
+		chann[k]=make(chan string)
+	}
+	// Launch clients
+	for i:=0;i<n;i++ {
+		go Client(chann[i],cmd[i],cd[i])
+	}
+	var R [n]string
+	for j:=0;j<n;j++ {
+		R[j]=<-chann[j]
+		if j==8 {
+			r:=strings.Fields(R[j])
+			R[j]=r[0]
+		}				
+		if R[j]!=E[j] {			
+			t.Error("Expected and Received values are:\r\n",E[j],R[j])
+		}
+	}
+	
+}
+
+//PASSED
+//Checking multiple commands send by single user
+func TestMultipleCmds(t *testing.T){
+	t.Parallel()
+	chann:=make(chan string)
+	const n int=4
+	cmd1:="set abc 10 10\r\ndata1\r\ngetm abc\r\ndelete abc\r\ngetm abc\r\n"
+	E:=[]string{"OK","VALUE 10 10\r\ndata1\r\n","DELETED","ERRNOTFOUND"}
+	//E:=[]string{"OK","VALUE 10 10\r\n"}
+	go Client(chann,cmd1,"cmd1")
+	for i:=0;i<n;i++ {
+		R:=<-chann
+		Rline:=strings.Split(R,"\r\n")
+		r:=strings.Fields(Rline[0])
+		if i==0 {		
+			R=r[0]
+		}else if i==1 {
+			R=r[0]+" "+r[2]+" "+r[3]+"\r\n"+Rline[1]+"\r\n"
+		}else{
+			R=Rline[0]
+		}
+		if R!=E[i] {			
+			t.Errorf("Expected and Received values are:\r\n%v\n%v",E,R)
+		}
+	}
+
+}
+
+
+/*
+func TestNoReply(t *testing.T){
+	chann := make(chan string)
+	cmd := "set mnp 10 10 noreply\r\nabcdefjg\r\nget mnp"
+	go Client(chann,cmd,"cmd")
+	Eset3:="OK"
+	fmt.Println("before reading")
+	Rset3 := <-chann
+	fmt.Println("After reading")
+	r:=strings.Fields(Rset3)
+	Rset3=r[0]
+	if strings.EqualFold(Eset3, Rset3) != true {
+		t.Error("Expected and Received values are:\r\n", Eset3, Rset3)
+	}
+}*/
+
+
+
+
+//Problem: Order of goroutines is not known so rand.Intn output is varying--No need to check version
+//to test noreply
+//to test concurrency
+//to test numbytes
+// to test all the ERR replies--DONE
+//to test check n expire deletion--DONE
+// to test expiry left in getm--DONE
+// when shud ERR_INTERNAL come?
+
